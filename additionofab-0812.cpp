1409f2ba1117c4ed37efde90b28511f819f61463
ksvsri <144914086+ksvsri@users.noreply.github.com>
2024-04-26 10:03:16 +0530

    Add files via upload

---
+++ additionofab-0812.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+ int addition(int A, int B){
+        // code here
+        return A+B;
+    }
+int main()
+{
+	int a,b;
+	cin>>a>>b;
+	cout<<addition(a,b);
+	return 0;
+}
